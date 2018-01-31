a1fbecc608b573e436355645d7a94d2e2340fa98
Javeedj2ee <35997475+Javeedj2ee@users.noreply.github.com>
2018-01-31 20:44:20 +0530

    Add files via upload

---
+++ course-api/src/main/java/io/javeed/springbootstarter/CourseApiApp.java
@@ -0,0 +1,14 @@
+package io.javeed.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+class CourseApiApp {
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApiApp.class, args);
+
+	}
+
+}
---
+++ course-api/src/main/java/io/javeed/springbootstarter/topic/TopicService.java
@@ -0,0 +1,39 @@
+package io.javeed.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+ 
+@Service
+public class TopicService {
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("Java", "J", "CoreJava_Description"),
+			new Topic("Spring", "S", "Spring_Description"), new Topic("Hibernate", "H", "Hibernate_Description")));
+
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+			}
+			return;
+		}
+	}
+
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}
